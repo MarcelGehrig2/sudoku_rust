7a8beae649aad298e8c7ffd237d20616ea5cc470
leuenmat <matze@ik.me>
2022-08-25 14:02:43 +0200

    clean up main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use crate::read_file::read_input;
-
 mod read_file;
-
-
 mod board;
-
 
 fn main() {
     
     let mut my_board = board::Board::new();
+
+    read_file::read_input();
     
     my_board.set_mutable(0, 0, 8);
     my_board.set_unmutable(0, 1, 9);
-
     my_board.print();
-    read_input();
 }
