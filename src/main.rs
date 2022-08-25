2c7d5b3b2a8d259892eb6527bf24435e2838a204
Marcel Gehrig <marcel.gehrig@noser.com>
2022-08-25 13:34:27 +0200

    types finished

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@ mod types;
 
 fn main() {
     
-  //  let mut board = types::types::Cell {value: 10, fixed: false};
     
-    let mut board = types::types::Board {state: [[0; 9]; 9]};
+    let mut board = types::types::Board::new();
     
-    board.state[1][5] = 1;
+    board.SetMutable(0, 0, 8);
+    board.SetUnmutable(0, 1, 9);
 
- //   board.state[0][0].cell.value = 0;
 
     println!("Hello, world! {:?}", board);
 }
--- src/types.rs
+++ src/types.rs
@@ -5,23 +5,32 @@ pub mod types {
     pub const HEIGHT: usize = 9;
     pub const LENGTH: usize = 9;
 
-    #[derive(Debug)]
-    pub struct Cell {
+    #[derive(Debug, Copy, Clone)]
+    struct Cell {
        pub value: u8,
-       pub fixed: bool
+       pub mutable: bool
     }
 
     #[derive(Debug)]
     pub struct Board {
-      //  pub state: [[u8; 9]; 9]
-        pub state: [[u8; LENGTH]; HEIGHT],
-    //    pub cell: Cell,
+        cells: [[Cell; LENGTH]; HEIGHT],
     }
 
     impl Board {
-        fn SetUnmuatable(&self, x: usize, y: usize, value: u8) {
-            self.state[x][y] = value;
+        pub fn new() -> Self {
+            let empty_cell: Cell = Cell {value: 0, mutable: false};
+            Self{cells: [[empty_cell; 9]; 9]}
         }
-    }
+
+        pub fn SetUnmutable(&mut self, x: usize, y: usize, value: u8) {
+            self.cells[x][y].value = value;
+            self.cells[x][y].mutable = false;
+       }
+
+       pub fn SetMutable(&mut self, x: usize, y: usize, value: u8) {
+           self.cells[x][y].value = value;
+           self.cells[x][y].mutable = true;
+      }
+   }
 
 }
